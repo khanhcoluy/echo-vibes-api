8c5b422b8e2a10d854903fa2f1f859b91fcbe180
Khanh Nguyen <khanhcoluy@gmail.com>
2023-07-23 15:51:56 +0700

    Initialize EchoVibes services repo

---
+++ src/main/java/apiservices/echovibesapp/EchoVibesAppApplication.java
@@ -0,0 +1,13 @@
+package apiservices.echovibesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoVibesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchoVibesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apiservices/echovibesapp/EchoVibesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package apiservices.echovibesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchoVibesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
