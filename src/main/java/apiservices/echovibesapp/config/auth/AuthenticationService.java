ea5e06d4aedfcc91fa50fc5287d966ed77eb1238
Khanh Nguyen <khanhcoluy@gmail.com>
2023-09-29 23:23:10 +0700

    - Implemented Spring Security with jwtToken - Fix issue when authenticate

--- src/main/java/apiservices/echovibesapp/config/auth/AuthenticationService.java
+++ src/main/java/apiservices/echovibesapp/config/auth/AuthenticationService.java
@@ -1,9 +1,12 @@
 package apiservices.echovibesapp.config.auth;
 
+import apiservices.echovibesapp.models.user.Role;
 import apiservices.echovibesapp.models.user.User;
 import apiservices.echovibesapp.repository.UserRepository;
 import apiservices.echovibesapp.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ public class AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -20,6 +24,7 @@ public class AuthenticationService {
                 .lastName(request.getLastName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
                 .build();
 
         userRepository.save(user);
@@ -30,5 +35,18 @@ public class AuthenticationService {
                 .build();
     }
 
-    // Todo: Implement authenticate controller
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
 }
--- src/main/java/apiservices/echovibesapp/security/JwtAuthenticationFilter.java
+++ src/main/java/apiservices/echovibesapp/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,9 +23,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
